efd7b8e961c2036c1ba56597576ca2c20f7e774c
Luke Marren <lukemarren173@gmail.com>
2024-11-02 22:47:08 -0500

    Start chapter 2

--- analysing-data-using-linear-models/adulm-chapter2.R
+++ analysing-data-using-linear-models/adulm-chapter2.R
@@ -5,5 +5,21 @@
 ##------------------------------------------------------------------------------
 
 ##------------------------------------------------------------------------------
-## Section 1: Units, variables, and the data matrix
+## Section 1: Inference about a mean
 ##------------------------------------------------------------------------------
+
+# We would like to infer from a discrete number of measurements what the mean of the variable is had we measured continuously
+
+# Problem of stat inference is when you want to say something about that complete data set (the population), but you only observe a relatively small portion of the data (the sample)
+
+##------------------------------------------------------------------------------
+## Section 2: Sampling distribution of mean and variance
+##------------------------------------------------------------------------------
+
+# Sampling distribution of the sample mean - distriubtion of means of a bunch of samples
+
+# The mean of a sample is on average a good estimate of the pop mean
+
+# We can also have the sampling distribution of the sample variance
+
+# population - all values, both observed and unobserved
